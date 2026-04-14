4b760f9f4f55345e82dfb8b6b072a724c89cbca1
mconde422 <mconde043@gmail.com>
2026-04-14 15:53:27 +0000

    ok

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,8 @@ app.use(cors({
       'http://localhost:5500',
       'http://127.0.0.1:5500',
       'http://localhost:3000',
-      'http://127.0.0.1:3000'
+      'http://127.0.0.1:3000',
+      
     ].filter(Boolean);
 
     // Autoriser les requêtes sans origin (ex: Postman, mobile apps)
